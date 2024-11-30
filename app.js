1c33fd89e1f055a7f789bd5dd524f39552361a25
9ian9 <108910700+9ian9@users.noreply.github.com>
2024-12-01 00:23:04 +0700

    can run

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static('public'));
 
@@ -18,17 +18,17 @@ app.use(mainRoutes);
 app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.render('error');
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(port, () => {
-    console.log('The application is running on localhost:3000!')
+    console.log('The application is running on http://localhost:3000')
 });
